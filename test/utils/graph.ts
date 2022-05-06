44c46874b7f46728a0a53e48ff187d4fcddccc48
meho <michal.t1506@gmail.com>
2022-05-06 16:26:01 +0200

    add test command that runs dockerize tests

--- test/utils/graph.ts
+++ test/utils/graph.ts
@@ -85,8 +85,6 @@ export const waitForGraphSync = async ({
         }`,
       });
 
-      console.log({ indexingStatusForCurrentVersion });
-
       if (
         indexingStatusForCurrentVersion.synced &&
         indexingStatusForCurrentVersion.chains[0].latestBlock.number >=
