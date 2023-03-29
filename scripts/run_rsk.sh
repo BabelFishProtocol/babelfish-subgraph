b59150b9eeea430adb944fdb8b9cf2ec457f7f54
BetsyBraddock <k.shall0507+mutant@gmail.com>
2023-03-29 16:32:40 +0300

    Fixed url for deployment scripts

--- scripts/run_rsk.sh
+++ scripts/run_rsk.sh
@@ -5,8 +5,8 @@ yarn codegen
 docker-compose up -d postgres && sleep 10 && docker-compose up -d
 echo "Waiting for 1 minutes..."
 sleep 1m
-yarn create-local
+yarn create-rsk
 echo "Waiting for 1 minutes..."
 sleep 1m
-yarn deploy-local
+yarn deploy-rsk
 
--- scripts/run_rskTestnet.sh
+++ scripts/run_rskTestnet.sh
@@ -6,7 +6,7 @@ yarn codegen
 docker-compose up -d postgres && sleep 10 && docker-compose up -d
 echo "Waiting for 1 minutes..."
 sleep 1m
-yarn create-local
+yarn create-rskTestnet
 echo "Waiting for 1 minutes..."
 sleep 1m
-yarn deploy-local
+yarn deploy-rskTestnet
